ba262056680c379ab7281d991093b8c0ebaccec5
Fábio Monteiro <fabio.monteiro.21@hotmail.com>
2022-01-27 13:32:14 -0300

    Department entity

---
+++ src/application/Program.java
@@ -0,0 +1,11 @@
+package application;
+
+import com.daojdbc.model.entities.Department;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Department dep = new Department(1, "Books");
+        System.out.println(dep);
+    }
+}
